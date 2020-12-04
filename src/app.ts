a09b15822fdbdb7c11824d2ecbc8382de2359a79
Marc <marc.magdi@blink22.com>
2020-12-04 13:44:14 +0200

    chore: update to heroku port

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import { Database } from "./db";
 import Logger from "./utils/Logger";
 
 const app = express();
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(morgan("combined"));
 app.use("/api", routes);
